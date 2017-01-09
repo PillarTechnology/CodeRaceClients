a34a9e81cca414eae421249ba6c92d00517db740
Biju Rajan <brajan@Bijus-MacBook-Pro.local>
2017-01-09 16:32:30 -0500

    Adding in IP as parameter into the setup script

--- romanNumeralsJava/race.sh
+++ romanNumeralsJava/race.sh
@@ -1,6 +1,6 @@
 while true;
   do
     clear;
-    gradle clean accept report --continue -Pracer=$RACER_NAME;
+    gradle clean accept report --continue -Pracer=$RACER_NAME -PserverIP=$SERVER_IP;
     sleep 5;
   done;
