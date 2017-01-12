a15d1ddb496376a4c4e677dccc534fa8513cc0ae
Gregory Haskins <greg@greghaskins.com>
2017-01-12 12:57:18 -0500

    Add instructions for PrimeFactorsJS

--- PrimeFactorsJS/src/primeFactors.js
+++ PrimeFactorsJS/src/primeFactors.js
@@ -1,6 +1,18 @@
+/*
+Factorize a positive integer number into its prime factors.
+
+For example:
+
+	 2 -> [2]
+	 3 -> [3]
+	 4 -> [2,2]
+	 6 -> [2,3]
+	 9 -> [3,3]
+	12 -> [2,2,3]
+	15 -> [3,5]
+*/
+
 var primeFactors = function(number){
-  var primes = [0];
-  if(number === 1){
-    return primes;
-  }
+  var primes = [];
+  return primes;
 }
