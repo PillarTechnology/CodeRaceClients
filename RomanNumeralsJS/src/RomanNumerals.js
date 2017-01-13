8cbcd80659a3dcfacce166b5ec7d1f4d59b29f93
Gregory Haskins <greg@greghaskins.com>
2017-01-13 09:11:19 -0500

    Add roman numerals instructions

--- RomanNumeralsJS/src/RomanNumerals.js
+++ RomanNumeralsJS/src/RomanNumerals.js
@@ -1,7 +1,33 @@
+/*
+Given a Roman number as a string (eg "XX") determine
+its integer value (eg 20).
+
+Examples:
+
+   "I" -> 1 |    "X" -> 10 |
+  "II" -> 2 |   "XX" -> 20 |  
+ "III" -> 3 |  "XXX" -> 30 | 
+  "IV" -> 4 |   "XL" -> 40 |  
+   "V" -> 5 |    "L" -> 50 |    
+  "VI" -> 6 |   "LX" -> 60 |  
+ "VII" -> 7 |  "LXX" -> 70 |  
+"VIII" -> 8 | "LXXX" -> 80 | 
+  "IX" -> 9 |   "XC" -> 90 |
+                "C" -> 100 |
+
+
+  "XCIX" -> 99   ("XC" -> 90 + "IX" -> 9)
+ "XLVII" -> 47   ("XL" -> 40 + "VII" -> 7)
+ "LXIV"  -> 64   ("L" -> 50 + "X" -> 10 + "IV" -> 4)
+
+*/
+
+
 function romanNumerals() {
 
 };
 
 romanNumerals.prototype.convert = function(romanChar){
+	// your code goes here
 	return 0;
 };
