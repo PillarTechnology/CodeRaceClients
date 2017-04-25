bb56aee18db515cde2873366a0d2830a6b779693
Gabriel <gsheeley@pillar.com>
2017-04-25 10:56:25 -0400

    Fixed two scripts and the RNJ solution.

--- romanNumeralsJava/RomanNumeralsJavaSolution.java
+++ romanNumeralsJava/RomanNumeralsJavaSolution.java
@@ -1,30 +1,31 @@
 package com.coderace;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class RomanNumerals {
   public static int convert(String romanChar) {
-	int ouput = 0;
-  Map < String, Integer > R = new HashMap<>();
-  R.add('M', 1000);
-  R.add('D', 500);
-  R.add('C', 100);
-  R.add('L', 50);
-  R.add('X', 10);
-  R.add('V', 5);
-  R.add('I', 1);
-
-  int i = 0;
-  while(i < romanChar.length()) {
-    if(i == romanChar.length - 1 || R[romanChar[i]] >= R[romanChar[i + 1]]) {
-      output += R[romanChar[i]];
-    } else {
-      output += R[romanChar[i + 1]] - R[romanChar[i]];
-      i++;
+	int output = 0;
+        Map<Character, Integer> R = new HashMap<>();
+        R.put('M', 1000);
+        R.put('D', 500);
+        R.put('C', 100);
+        R.put('L', 50);
+        R.put('X', 10);
+        R.put('V', 5);
+        R.put('I', 1);
+        
+        int i = 0;
+        while (i < romanChar.length()) {
+            if(i == romanChar.length() - 1 || R.get(romanChar.charAt(i)) >= R.get(romanChar.charAt(i + 1))) {
+                output += R.get(romanChar.charAt(i));
+            } else {
+                output += R.get(romanChar.charAt(i + 1)) - R.get(romanChar.charAt(i));
+                i++;
+            }
+            i++;
+        }
+        return output;
     }
-    i++;
-  }
-  return output;
-}
 
 }
